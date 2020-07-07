5cf9a3aa3bce4ae9c888c7066b304fe92279f010
Sundar Palani <Sundar.Palani@analog.com>
2020-07-07 16:58:47 -0400

    updating message type from String to sensor_msgs/Imu

--- src/adi_imu_node.cpp
+++ src/adi_imu_node.cpp
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n("~");
     
     /* create publisher */
-    ros::Publisher ad_imu_pub = n.advertise<std_msgs::String>("data_raw", 1000);
+    ros::Publisher ad_imu_pub = n.advertise<sensor_msgs::Imu>("data_raw", 1000);
 
     /* setting data output rate */
     ros::Rate loop_rate(10);
@@ -53,32 +53,56 @@ int main(int argc, char **argv)
     float gyroLSB  = (4 * 10000 * 0.00625 / 655360000) * ( PI / 180); /* 0.00625 deg / 2^16 */
     float tempLSB = (1.0/140);
 
+    double accelX, accelY, accelZ, gyroX, gyroY, gyroZ;
+    adi_imu_BurstOutput_t out;
+    // char imuout[250];
+
     int count = 0;
     while (ros::ok())
     {
-        std_msgs::String msg;
+        sensor_msgs::Imu msg;
 
-        /* 4. Burst read 10 samples */
-        printf("\nPerforming burst read..\n");
-        adi_imu_BurstOutput_t out;
-        char imuout[250];
         for (int i=0; i<10; i++){
             if ((ret = adi_imu_ReadBurst(&imu, &out)) >= 0) {
-                double accelX = out.accl.x * acclLSB;
-                double accelY = out.accl.y * acclLSB;
-                double accelZ = out.accl.z * acclLSB;
-                sprintf(imuout, "sys_status=%x, temp=%fC, accX=%f, accY=%f, accZ=%f, gyroX=%f, gyroY=%f, gyroZ=%f datacnt_Or_ts=%d crc=0x%x", out.sysEFlag, 25 + out.tempOut * tempLSB, accelX, accelY, accelZ, out.gyro.x * gyroLSB, out.gyro.y * gyroLSB, out.gyro.z * gyroLSB, out.dataCntOrTimeStamp, out.crc);
+                accelX = out.accl.x * acclLSB;
+                accelY = out.accl.y * acclLSB;
+                accelZ = out.accl.z * acclLSB;
+                gyroX = out.gyro.x * gyroLSB;
+                gyroY = out.gyro.y * gyroLSB;
+                gyroZ = out.gyro.z * gyroLSB;
+                // sprintf(imuout, "sys_status=%x, temp=%fC, accX=%f, accY=%f, accZ=%f, gyroX=%f, gyroY=%f, gyroZ=%f datacnt_Or_ts=%d crc=0x%x", out.sysEFlag, 25 + out.tempOut * tempLSB, accelX, accelY, accelZ, gyroX, gyroY, gyroZ, out.dataCntOrTimeStamp, out.crc);
                 // printf("Pitch = %f deg \n", 180 * atan2(accelX, sqrt(accelY*accelY + accelZ*accelZ))/PI);
                 // printf("Roll = %f deg\n", 180 * atan2(accelY, sqrt(accelX*accelX + accelZ*accelZ))/PI);
             };
         }
-        msg.data = imuout;
-        ROS_INFO("%s", imuout);
+        ROS_INFO("IMU status: %d  data count: %d  read cnt: %d\n", out.sysEFlag, out.dataCntOrTimeStamp, count);
+
+        /* prepare imu sensor message */
+        msg.header.seq = count;
+        msg.header.stamp.sec = 0;
+        msg.header.stamp.nsec = 0;
+        msg.header.frame_id = "adi_imu";
+
+        msg.orientation.x = 0;
+        msg.orientation.y = 0;
+        msg.orientation.z = 0;
+        msg.orientation.w = 0;
+
+        msg.orientation_covariance = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+        msg.angular_velocity_covariance = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+        msg.linear_acceleration_covariance = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+        msg.angular_velocity.x = gyroX;
+        msg.angular_velocity.y = gyroY;
+        msg.angular_velocity.z = gyroZ;
+        msg.linear_acceleration.x = accelX;
+        msg.linear_acceleration.y = accelY;
+        msg.linear_acceleration.z = accelZ;
+
         ad_imu_pub.publish(msg);
         ros::spinOnce();
         loop_rate.sleep();
         ++count;
     }
-    
     return 0;
 }
