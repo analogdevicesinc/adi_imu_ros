647fb5825fcda2ed73aa4959c7cf58af1d8f9e5f
Audren Cloitre <audren.cloitre@analog.com>
2020-07-08 15:37:42 -0400

    Initial commit. Refactored code into classes. Added ROS params

---
+++ include/adi_imu_ros/adi_imu_ros.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "ros/ros.h"
+#include "ros/time.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Imu.h"
+
+#include "adi_imu_driver.h"
+#include "spi_driver.h"
+#include <cmath>
+#include <fstream>
+#include <string>
+
+class AdiImuRos
+{
+private:
+	// ROS variables
+  ros::Publisher  _pub_imu;
+  ros::NodeHandle _nh;
+  std::string _imu_frame;
+  // IMU variables
+	adi_imu_Device_t _imu;
+  adi_imu_BurstOutput_t _data;
+  uint16_t _count;
+  // Logger and publisher variables
+  bool _save_to_file;
+  std::ofstream _csv_stream;
+  std::string _msg_type;
+
+public:
+	AdiImuRos(const ros::NodeHandle nh);
+  ~AdiImuRos()
+  {
+    if(_csv_stream.is_open())
+      _csv_stream.close();
+  };
+  void run(void);
+  // void publish_std_msg();
+  // void publish_adi_msg();
+  // void save_to_file(const ros::Time t1);
+};
+
